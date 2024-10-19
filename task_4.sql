618f62e73c6e63ca503c244393f3c4850c6d87c5
Reham Habib <rehamhabib97@gmail.com>
2024-10-19 19:00:00 +0400

    edited

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,8 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Query the information_schema to get the full description of the books table
+-- Query the INFORMATION_SCHEMA to get the full description of the Books table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
-FROM information_schema.columns
-WHERE table_name = 'Books'
-AND table_schema = 'alx_book_store';
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books'
+AND TABLE_SCHEMA = 'alx_book_store';
